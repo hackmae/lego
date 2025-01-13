0f7c970c1d7653feae721e086c9dfc6eb07fa03f
Mae Benaroche <maebenar@gmail.com>
2025-01-13 19:08:33 +0100

    todoo1

--- client/v1/index.js
+++ client/v1/index.js
@@ -29,6 +29,8 @@ console.log(MY_FAVORITE_DEALERS[0]);
 // 0. I have 2 favorite lego sets shopping communities stored in MY_FAVORITE_DEALERS variable
 // 1. Create a new variable and assign it the link of the lego set with the highest reduction I can find on these 2 websites
 // 2. Log the variable
+var highestReduction = 'https://www.dealabs.com/bons-plans/75-sur-la-gamme-lego-mario-parthenay-79-2972397';
+console.log(highestReduction);
 
 /**
  * 🧱
@@ -42,6 +44,7 @@ console.log(MY_FAVORITE_DEALERS[0]);
 // 🎯 TODO 2: Number of deals
 // 1. Create a variable and assign it the number of deals
 // 2. Log the variable
+
 
 // 🎯 TODO 3: Website name
 // 1. Create a variable and assign it the list of shopping community name only
