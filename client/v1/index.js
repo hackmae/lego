a23fe0a7aa8340b026e265e652f81bd38ea1a5da
Mae Benaroche <maebenar@gmail.com>
2025-01-13 19:09:17 +0100

    todoo2

--- client/v1/index.js
+++ client/v1/index.js
@@ -44,7 +44,8 @@ console.log(highestReduction);
 // 🎯 TODO 2: Number of deals
 // 1. Create a variable and assign it the number of deals
 // 2. Log the variable
-
+var numberOfDeals = deals.length;
+console.log(numberOfDeals);
 
 // 🎯 TODO 3: Website name
 // 1. Create a variable and assign it the list of shopping community name only
