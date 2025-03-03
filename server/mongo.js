ba4e41d318c58752d98773556e13cfbe460c34b9
Mae Benaroche <maebenar@gmail.com>
2025-03-03 14:55:23 +0100

    vintedJSONfile

---
+++ server/mongo.js
@@ -0,0 +1,4 @@
+const {MongoClient} = require('mongodb');
+const MONGODB_URI = 'mongodb+srv://Hackkmae:MANGOlogan159@hackmae.lzdgy.mongodb.net/?retryWrites=true&w=majority&appName=Hackmae';
+const MONGODB_DB_NAME = 'Lego';
+
